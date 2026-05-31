5885e0ee42d50cee02a90bc52611e6b858587308
helmesjo <helmesjo@gmail.com>
2026-05-31 19:51:24 +0200

    bdep new --package -l c++ -t lib,split,no-subdir,third-party libassert Updated manifest.

---
+++ libassert/src/assert.cxx
@@ -0,0 +1,17 @@
+#include <assert.hxx>
+
+#include <ostream>
+#include <stdexcept>
+
+using namespace std;
+
+namespace assert
+{
+  void say_hello (ostream& o, const string& n)
+  {
+    if (n.empty ())
+      throw invalid_argument ("empty name");
+
+    o << "Hello, " << n << '!' << endl;
+  }
+}
